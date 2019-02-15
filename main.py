ef09229690ce9ad19bd3b251f43e19900bd3cd6b
Chris Kingdon <ckchessmaster@gmail.com>
2019-02-15 14:45:59 -0500

    1st commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 10:40:47 2019
+
+@author: chris kingdon
+"""
+
+import time
+from world import World
+
+def main():
+    world = World()
+    
+    while True:
+        start = time.time()
+        world.tick()
+        end = time.time()
+        diff = end - start
+        
+        # Force each tick to take no less than 1/20th of a second
+        if diff < 0.05:
+            time.sleep(0.05 - diff)
+  
+
+          
+main()
---
+++ world.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 10:58:30 2019
+
+@author: ckche
+"""
+import time
+class World:
+    def __init__(self):
+        test2 = 1
+        
+    def tick(self):
+        test = 1
